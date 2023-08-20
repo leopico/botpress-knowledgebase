7e3de47aef92631facb45781d967b5a448c9ad25
leopico <linkoko.peceng@gmail.com>
2023-08-20 21:15:19 +0630

    first-commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="flex h-screen justify-center items-center">
+      <span className="text-3xl text-green-400 font-bold text-center">
+        Welcome from our metaverse Zurains!
+      </span>
+    </div>
+  );
+}
+
+export default App;
